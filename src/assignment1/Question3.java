57dec87482210a340326e3b561efd6811f330216
DELL <DELL@Shital>
2022-06-06 09:39:39 +0530

    Committed on 6 june

---
+++ src/assignment1/Question3.java
@@ -0,0 +1,39 @@
+package assignment1;
+
+public class Question3 {
+	
+	/* 	USABILITY TESTING SUGGESTIONS
+	 
+	 * UI ASTHETICS
+	 		 Website has moderate asthetic features from my point of view.
+	 		 Good things about Asthetics are
+	 		   -All fields are clearly visible
+	 		   -All headings are clear and self explanatory
+	 		   -It has consize content
+	 		 Things that can be improved according to me are
+	 		   -Headings could be kept simple(non Italic)
+	 		   -Background color could be of more decent/light green color
+	 		   -Size of some rows is unnecessarily kept large it can be scaled down
+	 		   -Under power supply heading one row at the top is kept blank it can be removed for better space utilization
+	 		   -Overall Space utility could be better
+	 		
+	 * UASABILITY
+	 		The overall usability is good 	
+	 		   -Order of options appearing on web page can be changed eg., Purpose of supply can mentioned on the top
+	 		    so that next options should appear according to that
+	 		   -We can also improve particular headings appearance after completing previous headings information so that 
+	 		    we can start filling form if it has been interrupted previously 
+	 		   -As we can see when we click on list of documents it direct us to another page rather than that we can provide that info 
+	 		    at the starting of form so that it can be easy for customer to fill form
+	 		    
+	 * Functionality
+	        Overall Functionality is also good
+	        	-The declaration of customer is mentioned 2 times it should be converted into one proper declaration
+	        	-We can also add Email validation functionality
+	        	-Total load functionality is very good that helps in understanding actual load(Calculate load)
+	        	
+	 		 
+	 
+	*/
+
+}
